78aabfc7ad2be84100a050078d203e224ae81e03
GypsyJR <ivan.demidov.01@list.ru>
2021-05-08 22:00:48 +0300

    Notifications

--- app/src/main/java/com/example/finema/MainActivity.kt
+++ app/src/main/java/com/example/finema/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.finema
 
+import android.app.AlarmManager
+import android.app.Notification
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.navigation.Navigation
 import com.example.finema.databinding.ActivityMainBinding
+import com.example.finema.ui.settings.NotificationService
 
 class MainActivity : AppCompatActivity() {
+    val NOTIFICATION_CHANNEL_ID = "10001"
+    private val default_notification_channel_id = "default"
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var service: Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +30,8 @@ class MainActivity : AppCompatActivity() {
         binding.topAppBar.setNavigationOnClickListener {
             binding.drawerLayout.open()
         }
+
+
 
         binding.navView.setNavigationItemSelectedListener {
             binding.drawerLayout.close()
@@ -31,10 +46,50 @@ class MainActivity : AppCompatActivity() {
 
                 R.id.settings -> Navigation.findNavController(this, R.id.fragment)
                     .navigate(R.id.action_global_fragmentSettings)
-
             }
 
             true
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+//        stopService(service)
+    }
+
+    override fun onStop() {
+        super.onStop()
+//        ContextCompat.startForegroundService(this, service)
+        scheduleNotification(getNotification(), 10000)
+    }
+
+    private fun scheduleNotification(notification: Notification, delay: Long){
+        service = Intent(this, NotificationService::class.java)
+        service.putExtra(NotificationService.NOTIFICATION_ID, 1)
+        service.putExtra(NotificationService.NOTIFICATION, notification)
+        val pendingIntent = PendingIntent.getBroadcast(this, 0, service,
+            PendingIntent.FLAG_UPDATE_CURRENT)
+        val futureMillis: Long = SystemClock.elapsedRealtime() + delay
+        val alarmManager: AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureMillis, pendingIntent)
+    }
+
+    private fun getNotification(): Notification {
+        val resultIntent = Intent(this, MainActivity::class.java)
+        val stackBuilder: TaskStackBuilder = TaskStackBuilder.create(this)
+        stackBuilder.addParentStack(MainActivity::class.java)
+        stackBuilder.addNextIntent(resultIntent)
+        val resultPendingIntent: PendingIntent =
+            stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        return NotificationCompat.Builder(this, default_notification_channel_id)
+            .setContentTitle("Стало скучно?")
+            .setContentText("Выбери себе фильм на вечер.")
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setAutoCancel(true)
+            .setChannelId(NOTIFICATION_CHANNEL_ID)
+            .setContentIntent(resultPendingIntent)
+            .build()
     }
 }
